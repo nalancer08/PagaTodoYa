f41aac8eb975db7f3f0ee8121d55a2ca1a478d22
Erick Sanchez <nuk.les@hotmail.com>
2018-02-19 13:11:05 -0600

    Added dialogs

--- app/src/main/java/com/pagatoexamen/ya/Adapter/RecargaActivity.java
+++ app/src/main/java/com/pagatoexamen/ya/Adapter/RecargaActivity.java
@@ -1,13 +1,17 @@
 package com.pagatoexamen.ya.Adapter;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Dialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.media.Image;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,6 +22,9 @@ import android.widget.TextView;
 
 import com.pagatoexamen.ya.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class RecargaActivity extends AppCompatActivity {
 
     private TextView back;
@@ -25,6 +32,8 @@ public class RecargaActivity extends AppCompatActivity {
     private EditText phone;
     private EditText money;
     private Button continue_button;
+
+    private Bitmap logo_bm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +60,8 @@ public class RecargaActivity extends AppCompatActivity {
 
         String marca = "ic_" + getIntent().getStringExtra("tipo");
         int id = getResources().getIdentifier(marca,"drawable", getPackageName());
-        Bitmap logo = BitmapFactory.decodeStream(getResources().openRawResource(id));
-        this.logo.setImageBitmap(logo);
+        this.logo_bm = BitmapFactory.decodeStream(getResources().openRawResource(id));
+        this.logo.setImageBitmap(this.logo_bm);
 
         this.back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,15 +80,90 @@ public class RecargaActivity extends AppCompatActivity {
                 final Dialog dialog = new Dialog(RecargaActivity.this);
                 dialog.setContentView(R.layout.dialog_recarga);
                 dialog.setCanceledOnTouchOutside(true);
-                //dialog.getWindow().setLayout(getX() - 20, getY() - threeRuleY(250));
+                dialog.getWindow().setLayout(getX() - 20, getY() - threeRuleY(250));
                 //dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_borders);
                 //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 //dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 //dialog.getWindow().getDecorView().setBackgroundResource(R.color.transparent);
                 dialog.show();
+
+                // Setting dialog
+                TextView hour = dialog.findViewById(R.id.hour);
+                hour.setText(Calendar.getInstance().getTime().toString());
+
+                TextView date = dialog.findViewById(R.id.date);
+                date.setText(new SimpleDateFormat("MMM d, yyyy").toString());
+
+                ImageView logo = dialog.findViewById(R.id.logo);
+                logo.setImageBitmap(logo_bm);
+
+                TextView phoneText = dialog.findViewById(R.id.phone);
+                phoneText.setText(phone.getText());
+
+                TextView mount = dialog.findViewById(R.id.mount);
+                mount.setText("$" + money.getText());
+
+                Button cancel = dialog.findViewById(R.id.cancel);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.cancel();
+                    }
+                });
+
+                Button acept = dialog.findViewById(R.id.acept);
+                acept.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.cancel();
+                    }
+                });
+
             }
         });
 
+    }
+
+    /**
+     * View helpers
+     **/
+
+    protected int threeRuleY(int value) {
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int heigth = size.y;
+
+        return (heigth * value) / 1794;
+    }
+
+    protected int threeRuleX(int value) {
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+
+        return (width * value) / 1000;
+    }
+
+    /** Dialog helpers **/
+
+    protected int getY() {
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.y;
+    }
+
+    protected int getX() {
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.x;
     }
 
     @SuppressLint("NewApi")
